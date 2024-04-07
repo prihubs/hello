0948413df78c09112cc311d2cc38bdb0b0b6a5bf
prihubs <pjgraphics1@gmail.com>
2024-04-07 17:39:48 +0100

    Implemented uploading access by Signed in users  only

---
+++ canAccess/uploadFunc.ca.php
@@ -0,0 +1,37 @@
+<?php
+
+echo '
+<div class="maiin">
+            <div class="main sticky">
+                <div class="sec1">
+                    <h1 class="sub">File_System</h1>
+                    <em class="ssub">Uploading your File with Ease</em>
+                </div>
+                <div class="space"></div>
+                <div class="messages">
+                    <?php include_once "ErrorHandler.php"; ?>
+                </div>
+
+                <div class="space"></div>
+                <div class="sec2">
+                    <form action="includes/makeFile.inc.php" method="post" enctype="multipart/form-data">
+                        <h2 class="sub2">Fill in all Required feilds!</h2>
+    
+                        <label for="name">Enter File Name</label> <br>
+                        <input type="text" name="name" class="inp" id="name"> <br>
+    
+                        <label for="title">Enter File Title</label> <br>
+                        <input type="text" name="title" class="inp" id="title"> <br>
+    
+                        <label for="desc">Enter File Description</label> <br>
+                        <input type="text" name="desc" class="inp" id="desc"> </p>
+
+                        <input type="file" name="file" class="inp file"> </p>
+    
+                        <input type="submit" name="submit" value="Upload" class="inp btn u">
+                        <input type="submit" name="reset" value="Reset" class="inp btn r">
+                    </form>
+                    <em class="ssub2"> Upload only jpg, png, jpeg</em>
+                </div>
+            </div>
+        </div> ';
--- index.php
+++ index.php
@@ -3,8 +3,9 @@
 require_once "includes/Dbh.php";
 
 session_start();
-$_SESSION['userId'] = 3;
-// $_SESSION['userType'];
+// $_SESSION['userId'] = 3;
+$_SESSION['usertype'] = "admin";
+$usertype = "userdd";
 // $_SESSION['user'];
 // $_SESSION['id'];
 
@@ -21,40 +22,15 @@ $_SESSION['userId'] = 3;
 <body>
 
     <div class="container">
-        <div class="maiin">
-            <div class="main sticky">
-                <div class="sec1">
-                    <h1 class="sub">File_System</h1>
-                    <em class="ssub">Uploading your File with Ease</em>
-                </div>
-                <div class="space"></div>
-                <div class="messages">
-                    <?php include_once "ErrorHandler.php"; ?>
-                </div>
+        <?php 
+            if($_SESSION['usertype'] === 'user' || 'admin'){
 
-                <div class="space"></div>
-                <div class="sec2">
-                    <form action="includes/makeFile.inc.php" method="post" enctype="multipart/form-data">
-                        <h2 class="sub2">Fill in all Required feilds!</h2>
-    
-                        <label for="name">Enter File Name</label> <br>
-                        <input type="text" name="name" class="inp" id="name"> <br>
-    
-                        <label for="title">Enter File Title</label> <br>
-                        <input type="text" name="title" class="inp" id="title"> <br>
-    
-                        <label for="desc">Enter File Description</label> <br>
-                        <input type="text" name="desc" class="inp" id="desc"> </p>
-
-                        <input type="file" name="file" class="inp file"> </p>
-    
-                        <input type="submit" name="submit" value="Upload" class="inp btn u">
-                        <input type="submit" name="reset" value="Reset" class="inp btn r">
-                    </form>
-                    <em class="ssub2"> Upload only jpg, png, jpeg</em>
-                </div>
-            </div>
-        </div>
+                if(isset($_SESSION['userId'])){
+                    require_once "canAccess/uploadFunc.ca.php";
+                }
+                echo $_SESSION['userId'];
+            }
+        ?>
 
         <div class="divider"></div>
 
